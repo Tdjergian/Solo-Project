600efa48c1dfcfa2fb07c249e143422a9acdd905
Tdjergian <152138733+Tdjergian@users.noreply.github.com>
2024-04-01 10:40:12 -0700

    Update home.js

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-  
+import { useSelector, useDispatch } from 'react-redux';  
+import { login } from '../slices/verificationSlice.js';
+
+
 
 const HomePage = props => {
     const [ words, setWords ] = useState('');
-    let navigate = useNavigate();
-
+    const navigate = useNavigate();
+    const loggedIn = useSelector(state => state.verification.loggedIn);
+   
     useEffect(()=>{
-        fetch('/api/')
+        fetch('/login')
     .then(res => res.json())
-    .then(res => setWords(res))
+    .then(res =>{
+      if(res === 'true'){
+        useDispatch(login())
+    })
     }, []);
   
     function reRoute () { 
@@ -17,15 +24,24 @@ const HomePage = props => {
      }
     
 
-    return (
+   
+      if(!loggedIn){
+      return (
+        <>
+          <div>
+            Loading Home page, please wait...
+          </div>
+        </>
+      )
+      }else {
+        return (
         <div>
             {words}
             <button onClick={reRoute}>
                 back
             </button>
-        
         </div>
-    )
+      )
 };
 
 export default HomePage; 
