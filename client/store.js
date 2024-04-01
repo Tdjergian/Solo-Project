080cc0407da47273ed9f6bb0d3f97986c22c1527
Tdjergian <152138733+Tdjergian@users.noreply.github.com>
2024-04-01 08:36:58 -0700

    Update store.js

--- client/store.js
+++ client/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import ticketSlice from './pages/slice';
+import ticketSlice from './slices/ticketSlice';
+import verificationSlice from './slices/verificationSlice'
 
 // we are adding composeWithDevTools here to get easy access to the Redux dev tools
 const store = configureStore(
   {
     reducer: {
-      tickets: ticketSlice
+      tickets: ticketSlice, 
+      verification: verificationSlice
     }
   }
   
