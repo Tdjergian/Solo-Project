4798631facb8c59b16ac55d7a6a6e6c7da34122f
Tdjergian <152138733+Tdjergian@users.noreply.github.com>
2024-04-01 08:26:50 -0700

    Update verificationSlice.js

--- client/slices/verificationSlice.js
+++ client/slices/verificationSlice.js
@@ -1,1 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+export const ticketSlice = createSlice({
+    name: 'verification', 
+    initialState: {
+        loggedIn : false,
+    }, 
+    reducers: {
+        verifyUser : ()=>{
+          
+        }
+      
