9de54749a6ec9d104133103301089d2cfcdf835d
Tdjergian <tdjerg@gmail.com>
2024-04-04 22:07:40 -0700

    added goToSignup

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import LoginBox from '../components/loginBox';
+import {  Button } from 'react-bootstrap';
+import { useNavigate } from "react-router";
 
 const LoginPage = props => {
     console.log('login page')
+
+    const navigate = useNavigate();
+
+    const goToSignUp = () => {
+        navigate('/signup')
+    }
+
     return (
         <>
             <LoginBox />
+            <Button onClick={goToSignUp}> Create new account </Button>
+
         </>
     )
 };
